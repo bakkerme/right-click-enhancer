9d26f2f56bcdedf2fad62ee6f3198198bca3dda6
Brandon Bakker <brandon@bdmd.com.au>
2018-10-28 09:13:30 +1000

    Add context menu when highlighted URL

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -5,8 +5,11 @@ function highlightHandler(e) {
   console.log("onup");
   const highlightedText = document.getSelection();
   if(highlightedText !== '') {
-      browser.runtime.sendMessage(null, {data: highlightedText});
+      console.log('text');
+      console.log(highlightedText);
+      browser.runtime.sendMessage(null, {data: highlightedText.toString()});
   } else {
+      console.log('no text');
       browser.runtime.sendMessage(null, {data: null});
   }
 }
--- src/js/navigate-to.js
+++ src/js/navigate-to.js
@@ -1,34 +1,36 @@
-browser.runtime.onMessage.addListener(contentMessageRecieved);
+import urlRegex from 'url-regex';
+let hasMenu = false;
 
-function contentMessageRecieved(e) {
+browser.runtime.onMessage.addListener(contentMessageReceived);
+
+function contentMessageReceived(e) {
+  const { data } = e;
   console.log('Got a message!');
-  // if(text !== '') {
-    
-  // } else {
-  //   browser.contextMenus.removeAll();
-  // }
+  console.log(data);
+
+  if (!data) {
+    hasMenu = false;
+    browser.contextMenus.removeAll();
+    return;
+  }
+
+  const isURL = urlRegex().test(data);
+  if (isURL) {
+    hasMenu = true;
+    browser.contextMenus.create({
+      id: "remove-me",
+      title: "Navigate To",
+      contexts: ["all"]
+    }, onClicked);
+  }
 }
-// browser.contextMenus.create({
-//       id: "remove-me",
-//       title: "Navigate To",
-//       contexts: ["all"]
-//     }, onClicked);
 
-//     function onClicked() {
-//       console.log("test");
-//       console.log("test2");
-//     }
-
+function onClicked() {
+      console.log("test");
+      console.log("test2");
+    }
 
 // "default_icon": {
-      // "20": "images/color-changer20.png",
-      // "40": "images/color-changer40.png"
-    // },
-
-  // "content_scripts": [{
-        // "matches": [
-                  // "<all_urls>"
-              // ],
-        // "js": ["js/content.js"],
-        // "run_at": "document_end"
-  // }]
+// "20": "images/color-changer20.png",
+// "40": "images/color-changer40.png"
+// },
